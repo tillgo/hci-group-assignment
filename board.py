ccda998d97b337a6c96f9ca1cddfc82f1f9697c5
Crayee <till.gohl@gmx.de>
2023-12-06 13:36:05 +0000

    cleanup

--- board.py
+++ board.py
@@ -2,6 +2,7 @@ from PyQt6.QtWidgets import QFrame
 from PyQt6.QtCore import Qt, QTimer, pyqtSignal, QPoint
 from PyQt6.QtGui import QPainter, QColor, QBrush
 
+from piece import Piece
 from piececonfig import PieceConfig
 
 
@@ -9,34 +10,31 @@ class Board(QFrame):  # base the board on a QFrame widget
     updateTimerSignal = pyqtSignal(int)  # signal sent when the timer is updated
     clickLocationSignal = pyqtSignal(str)  # signal sent when there is a new click location
 
-    # TODO set the board width and height to be square
-    boardWidth = 7  # board is 0 squares wide # TODO this needs updating
-    boardHeight = 7  #
+    boardWidth = 7  # board is 7 squares wide
+    boardHeight = 7  # board is 7 squares high
     timerSpeed = 1000  # the timer updates every 1 second
     counter = 10  # the number the counter will count down from
 
     def __init__(self, parent):
         super().__init__(parent)
+
+        self.timer = QTimer(self)  # create a timer for the game
+        self.timer.timeout.connect(self.timerEvent)  # connect timeout signal to timerEvent method
+        self.isStarted = False  # game is not currently started
+        self.boardArray: list[list[Piece]] = [[]]  # state of the board
+
         self.initBoard()
 
     def initBoard(self):
         """initiates board"""
-        self.timer = QTimer(self)  # create a timer for the game
-        self.timer.timeout.connect(self.timerEvent)  # connect timeout signal to timerEvent method
-        self.isStarted = False  # game is not currently started
         self.start()  # start the game which will start the timer
 
         self.boardArray = [[PieceConfig.NoPiece for _ in range(self.boardWidth)] for _ in
-                           range(self.boardHeight)]  # TODO - create a 2d int/Piece array to store the state of the game
+                           range(self.boardHeight)]
+
         self.boardArray[3][3] = PieceConfig.White
         self.boardArray[3][4] = PieceConfig.Black
         self.boardArray[4][4] = PieceConfig.White
-        self.printBoardArray()  # TODO - uncomment this method after creating the array above
-
-    def printBoardArray(self):
-        """prints the boardArray in an attractive way"""
-        print("boardArray:")
-        print('\n'.join(['\t'.join([str(cell) for cell in row]) for row in self.boardArray]))
 
     def mousePosToColRow(self, event):
         """convert the mouse click event to a row and column"""
@@ -44,8 +42,8 @@ class Board(QFrame):  # base the board on a QFrame widget
 
     def squareSize(self):
         """returns  the size (width and length of a square
-        The size is choosen by calculatin the maximum possible width and height and picking the smaller one,
-        so the quares are acutally squares and fit on the screen
+        The size is chosen by calculating the maximum possible width and height and picking the smaller one,
+        so the squares are actually squares and fit on the screen
         """
         max_width = int(self.contentsRect().width() / self.boardWidth)
         max_height = int(self.contentsRect().height() / self.boardHeight)
@@ -61,7 +59,7 @@ class Board(QFrame):  # base the board on a QFrame widget
         self.timer.start(self.timerSpeed)  # start the timer with the correct speed
         print("start () - timer is started")
 
-    def timerEvent(self, _):
+    def timerEvent(self):
         """this event is automatically called when the timer is updated. based on the timerSpeed variable """
         # TODO adapt this code to handle your timers
         if Board.counter == 0:
@@ -112,7 +110,6 @@ class Board(QFrame):  # base the board on a QFrame widget
                 if piece.color:
                     painter.save()
                     painter.setBrush(QColor(piece.color))
-                    print(piece.color)
                     painter.translate(col * self.squareSize(), row * self.squareSize())
                     # TODO draw some pieces as ellipses
                     # TODO choose your color and set the painter brush to the correct color
