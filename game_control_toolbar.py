87a25f2d7173c7969f8e4fe24cb587106b99c2db
siggi <simeon-holzapfel@gmx.de>
2023-12-21 12:02:51 +0100

    add game controls (still ugly)

---
+++ game_control_toolbar.py
@@ -0,0 +1,39 @@
+from PyQt6.QtGui import QAction, QIcon
+from PyQt6.QtWidgets import *
+
+
+class GameControlToolbar(QToolBar):
+    """
+    Toolbar containing game controls, like passing od redo a move
+    """
+
+    def __init__(self,parent, onUndo, onRedo, onReset, onPass):
+        # Toolbar actions and widgets
+        super().__init__(parent)
+
+
+        # Create Undo Action
+        self.undoAction = QAction(QIcon("./assets/icons/undo.png"), "Undo", parent)
+        self.undoAction.triggered.connect(onUndo)
+        self.undoAction.setToolTip("Undo Move")
+
+        # Create Redo Action
+        self.redoAction = QAction(QIcon("./assets/icons/redo.png"), "Redo", parent)
+        self.redoAction.triggered.connect(onRedo)
+        self.redoAction.setToolTip("Redo Move")
+
+        # Create reset action
+        self.resetAction = QAction(QIcon("./assets/icons/reset.png"), "Reset", parent)
+        self.resetAction.triggered.connect(onReset)
+        self.resetAction.setToolTip("Reset Game")
+
+        # create pass button
+        self.btnPass = QPushButton("Pass")
+        self.btnPass.clicked.connect(onPass)
+
+
+        # Add actions / widgets to toolbar
+        self.addAction(self.undoAction)
+        self.addAction(self.redoAction)
+        self.addAction(self.resetAction)
+        self.addWidget(self.btnPass)
--- go.py
+++ go.py
@@ -1,7 +1,8 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
-from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import Qt, QSize
 
 from board import Board
+from game_control_toolbar import GameControlToolbar
 from game_state import GameState
 from piececonfig import PieceConfig, getOpposite
 from rules import Rules
@@ -15,30 +16,82 @@ class Go(QMainWindow):
         self.gameHistory = []
         self.boardSize = 7
         self.currentPieceColor = PieceConfig.Black
-        gameState = GameState(None, [[PieceConfig.NoPiece for _ in range(self.boardSize)] for _ in
-                                 range(self.boardSize)], {PieceConfig.Black: 0, PieceConfig.White: 0}, False)
-        self.gameHistory.append(gameState)
-
+        self.defaultGameState = GameState(None, [[PieceConfig.NoPiece for _ in range(self.boardSize)] for _ in
+                                                 range(self.boardSize)], {PieceConfig.Black: 0, PieceConfig.White: 0},
+                                          False)
+        self.gameHistory.append(self.defaultGameState)
+        self.currentGameStateIndex = 0
 
         self.initUI()
 
+    def updateBoard(self):
+        """
+        Update boardArray and currentpiece color and redraw board
+        """
+        self.board.boardArray = self.gameHistory[self.currentGameStateIndex].boardArray
+        self.board.currentPieceColor = self.currentPieceColor
+        self.board.repaint()
+
     def onBoardFieldClicked(self, field):
-        currentGameState = self.gameHistory[-1]
+        tmpGameHistory = self.gameHistory[:self.currentGameStateIndex + 1]
+        currentGameState = tmpGameHistory[-1]
         if Rules.checkLegalMove(self.gameHistory, currentGameState.boardArray, field, self.currentPieceColor):
+            self.gameHistory = tmpGameHistory
             newBoardArray = [i.copy() for i in currentGameState.boardArray]
             newBoardArray[field.row][field.col] = self.currentPieceColor
             amountCaptured = Rules.try_captures(newBoardArray, self.currentPieceColor)
             newPrisoners = currentGameState.prisoners.copy()
-            newPrisoners[getOpposite(self.currentPieceColor)] = newPrisoners[getOpposite(self.currentPieceColor)] + amountCaptured
+            newPrisoners[getOpposite(self.currentPieceColor)] = newPrisoners[getOpposite(
+                self.currentPieceColor)] + amountCaptured
             self.gameHistory.append(GameState(self.currentPieceColor, newBoardArray, newPrisoners, False))
-            self.board.boardArray = newBoardArray
-            self.board.repaint()
-            self.currentPieceColor = PieceConfig.White if self.currentPieceColor is PieceConfig.Black \
-                else PieceConfig.Black
-            self.board.currentPieceColor = self.currentPieceColor
+            self.currentGameStateIndex += 1
+            self.currentPieceColor = getOpposite(self.currentPieceColor)
+            self.updateBoard()
+
+    def onUndoMove(self):
+        """Callback function for board object"""
+        if self.currentGameStateIndex > 0:
+            self.currentGameStateIndex -= 1
+
+        self.currentPieceColor = getOpposite(self.currentPieceColor)
+        self.updateBoard()
+
+    def onRedoMove(self):
+        """Callback function for board object"""
+        if self.currentGameStateIndex < len(self.gameHistory) - 1:
+            self.currentGameStateIndex += 1
+
+        self.currentPieceColor = getOpposite(self.currentPieceColor)
+        self.updateBoard()
+
+    def onResetGame(self):
+        """Callback function for board object"""
+        self.gameHistory = []
+        self.gameHistory.append(self.defaultGameState)
+        self.currentGameStateIndex = 0
+        self.currentPieceColor = PieceConfig.Black
+        self.updateBoard()
+
+    def onPass(self):
+        """
+        Callback function for board object
+        """
+        self.gameHistory = self.gameHistory[:self.currentGameStateIndex + 1]
+        lastGameState = self.gameHistory[-1]
+        newPrisoners = lastGameState.prisoners
+        # Give one stone to enemy player (rule if you pass)
+        newPrisoners[self.currentPieceColor] = newPrisoners[self.currentPieceColor] + 1
+        # Create new GameState with with unchanged boardArray, updated prisoners and isPass set to True
+        newGameState = GameState(self.currentPieceColor, lastGameState.boardArray, newPrisoners, True)
+        self.gameHistory.append(newGameState)
+        self.currentGameStateIndex += 1
+        self.currentPieceColor = getOpposite(self.currentPieceColor)
+        self.updateBoard()
 
     def onBoardHoverCheckLegalMove(self, field):
-        return Rules.checkLegalMove(self.gameHistory, self.gameHistory[-1].boardArray, field, self.currentPieceColor)
+        return Rules.checkLegalMove(self.gameHistory[:self.currentGameStateIndex + 1],
+                                    self.gameHistory[self.currentGameStateIndex].boardArray, field,
+                                    self.currentPieceColor)
 
     def getBoard(self):
         return self.board
@@ -48,13 +101,21 @@ class Go(QMainWindow):
 
     def initUI(self):
         """Initiates application UI"""
-        self.board = Board(self, self.gameHistory[-1].boardArray ,self.currentPieceColor, self.onBoardHoverCheckLegalMove)
+        self.board = Board(self, self.gameHistory[-1].boardArray, self.currentPieceColor,
+                           self.onBoardHoverCheckLegalMove)
         self.board.subscribeToFieldClicked(self.onBoardFieldClicked)
         self.setCentralWidget(self.board)
 
         self.scoreBoard = ScoreBoard()
         self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, self.scoreBoard)
         self.scoreBoard.make_connection(self.board)
+
+        # Create Toolbar
+        self.toolbar = GameControlToolbar(self, self.onUndoMove, self.onRedoMove, self.onResetGame, self.onPass)
+        self.toolbar.setIconSize(QSize(30, 30))
+        self.toolbar.setStyleSheet("QToolBar{spacing:15px;}")
+
+        self.addToolBar(Qt.ToolBarArea.BottomToolBarArea, self.toolbar)
 
         self.resize(800, 800)
         self.center()
